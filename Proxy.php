7e1888a4ee16fa98dd302ea58592853169351ab3
chen <569922982@qq.com>
2017-12-26 10:44:39 +0800

    proxy

--- Proxy.php
+++ Proxy.php
@@ -1,94 +1,57 @@
 <?php
+$listenHost = "0.0.0.0";
+$listenPort = "9988";
+$serv = new Swoole\Server($listenHost, $listenPort);
+$serv->set([
+    'worker_num' => 16,     //工作进程数量，决定最大并发量，一个进程可以hold n个连接
+    'daemonize' => false,  //是否以守护进程(后台)方式运行
+    // 'open_http_protocol' => true    //开启http协议处理
+]);
+$serv->on('start', function ($serv) use ($listenHost, $listenPort) {
+    echo "Server start on $listenHost:$listenPort ...";
+});
+$serv->on('connect', function ($serv, $fd) {
+    //do nothing
+});
+$serv->on('close', function ($serv, $fd) {
+    //do nothing
+});
+$serv->on('receive', function ($serv, $fd, $from_id, $data) {
+    $firstLine = explode("\r\n", $data, 2)[0];
+    $addr = explode(' ', $firstLine);
+    $http = $addr[0];
 
-class Proxy
-{
-    protected $server = null;
-    protected $http_methods = [
-        'GET', 'CONNECT'
-    ];
-    protected $http_clients = [];
-    protected $debug = true;
-
-    public function __construct(string $ip, int $port)
-    {
-        $this->server = new Swoole\Server($ip, $port, SWOOLE_BASE);
-        $this->server->on('Connect', [$this, 'onConnect']);
-        $this->server->on('Receive', [$this, 'onReceive']);
-        $this->server->on('Close', [$this, 'onClose']);
-        $this->server->start();
+    if (preg_match('/CONNECT/i', $http)) {
+        $urlData = explode(':', $addr[1]);
+        $host = $urlData[0];
+        $port = isset($urlData[1]) ? $urlData[1] : '443';
+        $client = new swoole_client(SWOOLE_TCP | SWOOLE_SSL, SWOOLE_SOCK_ASYNC);
+        echo "{$data}\n";
+    } else {
+        $urlData = parse_url($addr[1]);
+        $host = isset($urlData['host']) ? $urlData['host'] : false;
+        $port = isset($urlData['port']) ? $urlData['port'] : '80';
+        $client = new swoole_client(SWOOLE_SOCK_TCP, SWOOLE_SOCK_ASYNC);
     }
 
-    public function onConnect(Swoole\Server $server, int $fd, int $from_id)
-    {
-        $this->log($fd, 'onConnect');
-    }
+    if ($host) {
+        $client->on("connect", function ($cli) use ($data) {
+            $cli->send($data);
+        });
+        $client->on("receive", function ($cli, $data) use ($serv, $fd) {
+            $serv->send($fd, $data);
+        });
+        $client->on("error", function ($cli) {
+            echo "Connect failed\n";
+        });
+        $client->on("close", function ($cli) {
+            //do nothing
+        });
+        swoole_async_dns_lookup($host, function ($host, $ip) use ($client, $port) {
+            if (preg_match("/[\\d]{2,3}\\.[\\d]{1,3}\\.[\\d]{1,3}\\.[\\d]{1,3}/", $host)) $ip = $host;
+            $client->connect($ip, $port);
+        });
+    } else return;
 
-    public function onReceive(Swoole\Server $server, int $fd, int $reactor_id, string $data)
-    {
-        $this->log($fd, 'onReceive');
-        
-        if($ret = $this->parseHeader($data))
-        {
-            $host = $ret['host'];
-            $port = $ret['port'];
-            $method = $ret['method'];
-
-            $client = new Swoole\Client(SWOOLE_SOCK_TCP);
-            if (!$client->connect($host, $port, -1))
-            {
-                exit("connect failed. Error: {$client->errCode}\n");
-            }
-            $this->http_clients[$fd] = $client;
-
-            if($method == 'CONNECT')
-            {
-                $str = "HTTP/1.1 200 Connection Established\r\n\r\n";
-                $server->send($fd, $str);
-                $this->log($fd, ' https connect success');
-                return;
-            }
-
-            $client->send($data);
-            $server->send($fd, $client->recv());
-        }
-
-        if($client = ($this->http_clients[$fd] ?? null))
-        {
-            $client->send($data);
-            $response = $client->recv();
-            $server->send($fd, $response);
-        }
-    }
-
-    public function onClose(Swoole\Server $server, int $fd, int $reactorId)
-    {
-        $this->log($fd, ' onClose ');
-        $this->http_clients[$fd]->close();
-        unset($this->http_clients[$fd]);
-    }
-
-    protected function parseHeader(string $data)
-    {
-        $header_arr = explode("\n", $data);
-        $first_line = $header_arr[0] ?? '';
-        preg_match("/^[a-zA-Z]+\b/", $first_line, $match);
-        $method = $match[0] ?? '';
-        if(in_array($method, $this->http_methods))
-        {
-            preg_match("/\S+(?=\sHTTP)/", $first_line, $match);
-            $target = parse_url($match[0] ?? '');
-            $host = $target['host'];
-            $port = $target['port'] ?? 80;
-            return ['host' => $host, 'port' => $port, 'method' => $method];
-        }
-        return false;
-    }
-
-    protected function log(int $fd, $data)
-    {
-        if($this->debug)
-        {
-            echo sprintf("[fd: %d] %s\r\n", $fd, $data);        
-        }
-    }
-}
+});
+$serv->start();
